e15aad213db8715a5327de7e8662105f3ed89fb8
AnnRTL <alexandre.thomas@gmx.com>
2024-10-26 11:58:58 +0200

    Use test cache directory

--- tests/test.rs
+++ tests/test.rs
@@ -1,4 +1,4 @@
-use std::{env, process::Command};
+use std::{env, fs, path::PathBuf, process::Command};
 
 #[test]
 fn test_no_command() {
@@ -42,11 +42,29 @@ fn test_fetch() {
         "HYDRA_PROVIDERS",
         "git@github.com:Annrtl/hydra_registry.git",
     );
-    env::set_var("HYDRA_CACHE", "cache/");
-    let test_dir = env::current_dir().unwrap().join("tests");
+    
+    // Set cache directory
+    let tests_path = match fs::canonicalize(PathBuf::from("tests")) {
+        Ok(path) => path,
+        Err(err) => panic!("Failed to get test path: {}", err),
+    };
+
+    let cache_path = tests_path.join("cache");
+
+    env::set_var("HYDRA_CACHE", cache_path.clone());
+
+    // Check directory doesn't already exist
+    if std::path::Path::new(&cache_path).exists() {
+        println!("Removing cache directory");
+        match  std::fs::remove_dir_all(cache_path.clone()) {
+            Ok(_) => (),
+            Err(err) => panic!("Failed to remove cache directory: {}", err),
+        }
+    }
+
     // Lancer le binaire
     let output = Command::new(env!("CARGO_BIN_EXE_hydra"))
-        .current_dir(&test_dir)
+        .current_dir(&tests_path)
         .args(&["fetch"])
         .output()
         .expect("Failed to execute binary");
@@ -58,4 +76,14 @@ fn test_fetch() {
     let stdout = String::from_utf8(output.stdout).unwrap();
     println!("{}", stdout);
     assert!(stdout.contains("Done"));
+
+    // Check directory doesn't already exist
+    if std::path::Path::new(&cache_path).exists() {
+        println!("Removing cache directory");
+        match  std::fs::remove_dir_all(cache_path.clone()) {
+            Ok(_) => (),
+            Err(err) => panic!("Failed to remove cache directory: {}", err),
+        }
+    }
+
 }
